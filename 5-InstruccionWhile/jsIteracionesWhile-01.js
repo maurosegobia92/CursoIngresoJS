9b540c221eafbf8032525c124401da4fc7e6258a
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2021-08-04 09:43:03 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -1,7 +1,17 @@
 /*
+AUTOR:Mauro Nahuel Segobia.
+ENUCIADO: INTERACCIONES WHILE 1.
+Enunciado:
+al presionar el botón mostrar 10 repeticiones con números ASCENDENTE, desde el 1 al 10.
 al presionar el botón mostrar 10 repeticiones 
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	var contador;
+	contador=0;
+	while(contador<10)
+	{
+		contador=contador+1;
+		console.log(contador);
+	}	alert('iteración while');
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,4 +1,6 @@
 /*
+AUTOR:Mauro Nahuel Segobia.
+ENUCIADO: INTERACCIONES WHILE 1.
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
@@ -6,5 +8,11 @@ function mostrar()
 	var contador;
 	contador=0; 
 	alert('iteración while');
+	while(contador>10)
+	{
+		contador=contador+0;
+		console.log(contador);
+
+	}
 
 }//FIN DE LA FUNCIÓN
