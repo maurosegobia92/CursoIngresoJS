71f9c1d435d6d22166b09cdb02b63bb1273d1086
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-04 17:07:41 -0300

    Update 03-jsFerretePinturas.js

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -1,36 +1,33 @@
 /*
-AUTOR:Mauro Nahuel Segobia.
-ENUNCIADO: TP Ejercicio 3.
+Segobia Mauro DIV Z
+Ejercicio TP E/S 03
+
 3.	Para el departamento de Pinturas:
 A.	Al ingresar una temperatura en Fahrenheit debemos mostrar la temperatura en Centígrados con un mensaje concatenado (ej.: " 32 Fahrenheit son 0 centígrados").
 B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en Fahrenheit (ej.: "0  centígrados son 32 Fahrenheit ").
 */
 function FahrenheitCentigrados () 
 {
-	var temperaturaIng;
-	var farenhet;
-	var centigrados;
-	var farenAcenti;
-	temperaturaIng=parseInt(document.getElementById("txtIdTemperatura").value);
-	farenhet=temperaturaIng;
-	centigrados=temperaturaIng;
-	farenAcenti=(farenhet-32)*(5/9);
-	alert(farenhet+" grados farenheit equivalen a "+farenAcenti+" centigrados");
-	
+    var temperaturaFarenheitIngresada;
+    var centigrados;
 
+    
+    temperaturaFarenheitIngresada = document.getElementById("txtIdTemperatura").value;
+    temperaturaFarenheitIngresada = parseFloat(temperaturaFarenheitIngresada);
+    centigrados = (temperaturaFarenheitIngresada - 32) * 5/9;
+
+    alert(temperaturaFarenheitIngresada+" grados Farenheit son "+centigrados+" grados centigrados");
 
 }
 
 function CentigradosFahrenheit () 
 {
-	var temperaturaIng;
-	var farenhet;
-	var centigrados;
-	var centiAfaren;
-	temperaturaIng=parseInt(document.getElementById("txtIdTemperatura").value);
-	farenhet=temperaturaIng;
-	centigrados=temperaturaIng;
-	centiAfaren=(temperaturaIng*9/5)+32;
-	alert(centigrados+" centigrados equivalen a "+centiAfaren+" grados farenheit");
+	var temperaturaCentigradosIngresada;
+    var gradosFarenheit;
+
+    temperaturaCentigradosIngresada = document.getElementById("txtIdTemperatura").value;
+    gradosFarenheit = (temperaturaCentigradosIngresada * 9/5) + 32;
+
+    alert(temperaturaCentigradosIngresada+" grados centigrados son "+gradosFarenheit+" grados Farenheit");
 
 }
