03c72fd808c39ecc9ad771c70c02980b4d317428
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-17 09:59:25 -0300

    Update jsInstruccionSwitch-09.js

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,89 +1,92 @@
 /*
-AUTOR: Mauro Nahuel Segobia.
-Instrucción Switch 09 - 
+Segobia Mauro DIV Z
+Ejerecicio Instrucción 'Switch' 09
+
 Enunciado:
-una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, se pide el ingreso de una estacion del año 
-y localidad para vacacionar para poder calcular el precio final
+una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base, se pide el ingreso de una estacion del año y 
+localidad para vacacionar para poder calcular el precio final
 
 en Invierno: bariloche tiene un aumento del 20% cataratas y Cordoba tiene un descuento del 10% Mar del plata tiene un descuento del 20%
 
 en Verano: bariloche tiene un descuento del 20% cataratas y Cordoba tiene un aumento del 10% Mar del plata tiene un aumento del 20%
 
 en Otoño y Primavera: bariloche tiene un aumento del 10% cataratas tiene un aumento del 10% Mar del plata tiene un aumento del 10% y Cordoba tiene el precio sin descuento
-
-
 */
+
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	var destinoIngresado =txtIdDestino.value;
+	
+	var estacion;
+	var destino;
+	var precioBase;
+	var precioFinal;
 	var descuento;
-	var resultado;
-	alert(estacionIngresada);
+	var aumento;
+	
+	precioBase = 15000;
+	aumento = 0;
+	descuento = 0;
+	estacion = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
 
-	if(estacionIngresada=="Invierno")
+	switch(estacion)
 	{
-		switch(destinoIngresado)
-		{
-			case "Bariloche":
-			alert(15000*1.2);
-			break;
-
-			case "Cataratas":
-			case "Cordoba":
-			descuento=(15000*10)/100;
-			resultado=15000-descuento;
-			alert(resultado);
-			break;
-
-			default:
-			descuento=(15000*20)/100;
-			resultado=15000-descuento;
-			alert(resultado);
-			break;
-		}
-	}
-	else
-	{
-		if(estacionIngresada=="Verano")
-		{
-			switch(destinoIngresado)
+		case "Invierno":
+			switch(destino)
 			{
 				case "Bariloche":
-				descuento=(15000*20)/100;
-				resultado=15000-descuento;
-				alert(resultado);
+					aumento = 20;
 				break;
-
-				case "Cataratas":
-				case "Cordoba":
-				alert(15000*1.1);
+				
+				case "Mar del plata":
+					descuento = 20;
 				break;
+				
+				default:
+					descuento = 10;
+				break;	
+			}
+		break;
+		
+		case "Verano":
+			switch(destino)
+			{
+				case "Bariloche":
+					descuento = 20;
+				break;
+				
+				case "Mar del plata":
+					aumento = 20;
+				break;	
 
 				default:
-				alert(15000*1.2);
+					aumento = 10;
+				break;	
 			}
-
-		}
-		else
-		{
-			if(estacionIngresada=="Otoño"||estacionIngresada=="Primavera")
+		break;
+		
+		default:
+			if (destino!="Cordoba")
 			{
-				switch(destinoIngresado)
-				{
-					case "Bariloche":
-					case "Cataratas":
-					case "Mar del plata":
-					alert(15000*1.1);
-					break;
-
-					default:
-					alert(15000);
-					break;
-				}
+				aumento = 10;
 			}
-		}
+		break;	
+		
+		
 	}
+	
+	precioFinal = precioBase + (precioBase*aumento/100) - (precioBase*descuento/100);
+	alert (precioFinal);
+	
+		 
+		
+
+	
+
+
+
+
+
 
 
 }//FIN DE LA FUNCIÓN
