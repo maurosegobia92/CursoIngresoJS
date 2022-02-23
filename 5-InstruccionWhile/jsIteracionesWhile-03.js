8705fb4897269b80cd4ff04ce85a03b32f632182
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-23 11:43:00 -0300

    Update jsIteracionesWhile-03.js

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -1,8 +1,23 @@
 /*
-al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
+Segobia Mauro DIV Z
+Ejercicio IteracionesWhile 03
+
+Enunciado:
+al presionar el botón pedir la CLAVE (ayuda: es utn750)
+
+*/
 function mostrar()
 {
 	var claveIngresada;
 	claveIngresada = prompt("ingrese el número clave.");
+
+	while (claveIngresada!="utn750")
+	{
+		alert("Clave Incorrecta, vuelva a intentarlo");
+		claveIngresada = prompt("ingrese el número clave.");
+	}
+
+	alert ("Felicidades ingreso a su cuenta");
+
 	
 }//FIN DE LA FUNCIÓN
