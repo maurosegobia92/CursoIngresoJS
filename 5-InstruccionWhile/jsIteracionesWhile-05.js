cf41643a779ae37b147c42c8b76936ce302c90b1
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-23 12:19:42 -0300

    Update jsIteracionesWhile-05.js

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -1,12 +1,17 @@
 /*
-Al presionar el botón pedir un sexo
-'f' para femenino, 'm' para masculino.*/
+Segobia Mauro DIV Z
+Ejercicio Iteraciones While 05
+Enunciado:
+Al presionar el botón pedir un sexo 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
 	var sexoIngresado;
 	sexoIngresado = prompt("ingrese f ó m .");
 
-
+	while(sexoIngresado!="f" && sexoIngresado!="m")
+	{
+		sexoIngresado = prompt("Error, ingrese su sexo f ó m .");
+	}
 
 	txtIdSexo.value=sexoIngresado;
 }//FIN DE LA FUNCIÓN
