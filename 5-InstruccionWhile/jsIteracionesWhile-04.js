642941e8363db119401101b015c4f2e708c68ce3
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-23 12:19:01 -0300

    Update jsIteracionesWhile-04.js

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -1,9 +1,28 @@
 /*
-al presionar el botón 
-pedir un número entre 0 y 9 inclusive.*/
+Segobia Mauro DIV Z
+Ejercicio IteracionesWhile 04
+
+Enunciado:
+al presionar el botón pedir un número entre 0 y 9 inclusive.
+
+*/
 function mostrar()
 {
 	var numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
 	
+	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	numeroIngresado = parseInt(numeroIngresado);
+	
+	document.getElementById("txtIdNumero").value = numeroIngresado;
+	
+	while (numeroIngresado<0 || numeroIngresado>9)
+	{
+		prompt("Error, vuelva a intentarlo.\nIngrese un número entre 0 y 10.");
+		numeroIngresado = parseInt(numeroIngresado);
+	}
+	
+	
+
+	
+
 }//FIN DE LA FUNCIÓN
