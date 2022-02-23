7adaa0b82733e348fd2ed027075a0e73e3fca259
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-23 12:20:21 -0300

    Update jsIteracionesWhile-06.js

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,3 +1,11 @@
+/*
+Segobia Mauro DIV Z 
+Ejercicio Iteraciones While 06
+
+Enunciado:
+Al presionar el botón pedir 5 números e informar la suma acumulada y el promedio.
+*/
+
 function mostrar()
 {
 	var contador;
@@ -7,7 +15,19 @@ function mostrar()
 	contador=0;
 	acumulador=0;
 	
+	while(contador<5)
+	{
+		numeroIngresado = prompt("Ingrese un número");
+		numeroIngresado = parseInt(numeroIngresado);
+		acumulador=parseInt(acumulador);
+		acumulador = acumulador + numeroIngresado;
+
+		contador++;
+
+	}
 	
 	txtIdSuma.value=acumulador;
-	txtIdPromedio.value=acumulador/5;
+	txtIdPromedio.value=acumulador/5; // txtIdPromedio.value=acumulador/contador; También se puede. igual chequear.
+
+
 }//FIN DE LA FUNCIÓN
