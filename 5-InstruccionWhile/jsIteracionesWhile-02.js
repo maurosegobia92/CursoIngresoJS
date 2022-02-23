b7e1bf49d0982c638aad7a15ccb6c8d6aa7ea3ab
maurosegobia92 <52456580+maurosegobia92@users.noreply.github.com>
2022-02-23 11:42:10 -0300

    Update jsIteracionesWhile-02.js

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -1,18 +1,16 @@
 /*
-AUTOR:Mauro Nahuel Segobia.
-ENUCIADO: INTERACCIONES WHILE 1.
 al presionar el botón mostrar 10 repeticiones 
 con números DESCENDENTES, desde el 10 al 1.*/
 function mostrar()
 {
-	var contador;
-	contador=0; 
-	alert('iteración while');
-	while(contador>10)
-	{
-		contador=contador+0;
-		console.log(contador);
+	var i;
+	i = 10;
 
+	while(i>0)
+	{
+		document.write(i + "<br>");
+		i--;
+		
 	}
 
 }//FIN DE LA FUNCIÓN
